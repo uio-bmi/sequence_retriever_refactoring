38562777857a90fb133ac525ab6ae36f1b337392
Dmytro Titov <dmytro.titov@gmail.com>
2017-10-25 10:59:08 +0200

    Refactor the  main and test code.

--- sequence_retriever.py
+++ sequence_retriever.py
@@ -1,10 +1,14 @@
 from urllib.request import urlopen
+from os import linesep
+import encodings
 import urllib
 from xml.etree import ElementTree
 
+SEQUENCE = "SEQUENCE"
+DNA = "DNA"
+
 
 def fetch_sequence_from_web_service(source, chromosome, start_position, end_position):
-
     assert start_position >= 0
     assert end_position >= 0 and end_position > start_position
 
@@ -15,37 +19,35 @@ def fetch_sequence_from_web_service(source, chromosome, start_position, end_posi
     if source != "UCSC" and source != "TOGOWS":
         raise NotImplementedError("Cannot fetch sequences from %s" % source)
 
-    if source == "TOGOWS":
-        return _fetch_sequence_from_togows(source, chromosome, start_position, end_position)
-    elif source == "UCSC":
-        return _fetch_sequence_from_ucsc(source, chromosome, start_position, end_position)
+    return _fetch_sequence_from_togows(chromosome, start_position, end_position) \
+        if source == "TOGOWS" \
+        else _fetch_sequence_from_ucsc(chromosome, start_position, end_position)
 
 
-def _fetch_sequence_from_togows(source, chromosome, start_position, end_position):
-
-    url = "http://togows.org/api/ucsc/hg38/%s:%d-%d.fasta" % (chromosome, start_position, end_position)
-
+def _fetch_sequence(url):
     try:
         sequence = urlopen(url).read()
     except urllib.error.HTTPError:
-        raise Exception("Invalid query %s:%d:%d" % chromosome, start_position, end_position)
+        raise Exception("Invalid URL" + url)
+
+    return sequence
+
+
+def _fetch_sequence_from_togows(chromosome, start_position, end_position):
+    sequence = _fetch_sequence(
+        "http://togows.org/api/ucsc/hg38/%s:%d-%d.fasta" % (chromosome, start_position, end_position))
 
     # TOGOWS returns sequence with line breaks
     # Remove first line (header) and line breaks
-    sequence = ''.join(sequence.decode('utf8').split("\n")[1:])
+    sequence = "".join(sequence.decode(encodings.utf_8.getregentry().name).split(linesep)[1:])
     return sequence.lower()
 
 
-def _fetch_sequence_from_ucsc(source, chromosome, start_position, end_position):
-
-    url = "http://genome.ucsc.edu/cgi-bin/das/hg38/dna?segment=%s:%d,%d" % (chromosome, start_position, end_position)
-
-    try:
-        sequence = urlopen(url).read()
-    except urllib.error.HTTPError:
-        raise Exception("Invalid query %s:%d:%d" % chromosome, start_position, end_position)
+def _fetch_sequence_from_ucsc(chromosome, start_position, end_position):
+    sequence = _fetch_sequence(
+        "http://genome.ucsc.edu/cgi-bin/das/hg38/dna?segment=%s:%d,%d" % (chromosome, start_position, end_position))
 
     # UCSC the sequence wrapped in specially formatted xml
     xml_object = ElementTree.fromstring(sequence)
-    sequence_element = xml_object.find("SEQUENCE").find("DNA")
+    sequence_element = xml_object.find(SEQUENCE).find(DNA)
     return "".join(sequence_element.text.split()).lower()
--- tests.py
+++ tests.py
@@ -1,48 +1,30 @@
 import unittest
 from sequence_retriever import fetch_sequence_from_web_service
 
+
 # Test
 class SequenceRetrieverTester(unittest.TestCase):
-
     def setUp(self):
         self.valid_sources = ["UCSC", "TOGOWS"]
 
     def test_fetching_from_invalid_source_raises_exception(self):
-        self.assertRaises(
-            NotImplementedError,
-            fetch_sequence_from_web_service,
-            "invalid_test_source", "chr1", 1, 10
-        )
+        self.assertRaises(NotImplementedError, fetch_sequence_from_web_service, "invalid_test_source", "chr1", 1, 10)
 
     def test_fetching_from_invalid_start_raises_exception(self):
         for source in self.valid_sources:
-            self.assertRaises(
-                Exception,
-                fetch_sequence_from_web_service,
-                source, "chr1", -1, 10
-            )
+            self.assertRaises(Exception, fetch_sequence_from_web_service, source, "chr1", -1, 10)
 
     def test_fetching_from_invalid_end_raises_excecption(self):
         for source in self.valid_sources:
-            self.assertRaises(
-                Exception,
-                fetch_sequence_from_web_service,
-                source, "chr1", 1, -1
-            )
+            self.assertRaises(Exception, fetch_sequence_from_web_service, source, "chr1", 1, -1)
 
     def test_fetched_sequence_is_correct(self):
-
         for source in self.valid_sources:
             sequence = fetch_sequence_from_web_service(source, "chr2", 10000000, 10000005)
-            self.assertEqual(sequence, "ttaga",
-                             "Fetched sequence from %s does not match"
-                             "correct sequence" % source)
+            self.assertEqual(sequence, "ttaga", "Fetched sequence from %s does not match correct sequence" % source)
 
             sequence = fetch_sequence_from_web_service(source, "chr5", 2895722, 2895728)
-            self.assertEqual(sequence, "atggat",
-                             "Fetched sequence from %s does not match"
-                             "correct sequence" % source)
-
+            self.assertEqual(sequence, "atggat", "Fetched sequence from %s does not match correct sequence" % source)
 
 
 if __name__ == "__main__":
